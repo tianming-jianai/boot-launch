7e85147147d10990998fd887bd4bba8ea5ba522b
zsg <1724473740@qq.com>
2020-05-06 22:41:12 +0800

    springBoot整合FreeMarker

---
+++ src/main/java/com/zimug/bootlaunch/controller/TemplateController.java
@@ -0,0 +1,41 @@
+package com.zimug.bootlaunch.controller;
+
+import com.zimug.bootlaunch.pojo.ArticleVO;
+import com.zimug.bootlaunch.service.ArticleMyBatisRestService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Controller
+@RequestMapping("/template")
+public class TemplateController {
+
+    @Resource(name="articleMyBatisRestServiceImpl")
+    ArticleMyBatisRestService articleMyBatisRestService;
+
+    @GetMapping("/jsp")
+    public String index(String name, Model model) {
+
+        List<ArticleVO> articles = articleMyBatisRestService.getAll();
+
+        model.addAttribute("articles", articles);
+
+        //模版名称，实际的目录为：src/main/webapp/WEB-INF/jsp/jsptemp.jsp
+        return "jsptemp";
+    }
+
+    @GetMapping("/freemarker")
+    public String index(Model model) {
+
+        List<ArticleVO> articles = articleMyBatisRestService.getAll();
+
+        model.addAttribute("articles", articles);
+
+        //模版名称，实际的目录为：resources/templates/fremarkertemp.html
+        return "freemarkertemp";
+    }
+}
